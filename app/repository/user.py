7ca36283706fe18bf2839daf0fbd860262f9b7fc
JoaoFiorelli <joao.fiorelli.13@gmail.com>
2021-11-23 16:58:01 -0300

    Realizando o CRUD inicial.

--- app/repository/user.py
+++ app/repository/user.py
@@ -1,7 +1,8 @@
-from sqlalchemy.sql.functions import mode
 import schemas
 import models
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.functions import mode
+from fastapi import HTTPException, status
 
 
 def create(request: schemas.User, db: Session):
@@ -14,3 +15,40 @@ def create(request: schemas.User, db: Session):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+def get_all(db: Session):
+    users = db.query(models.User).all()
+    return users
+
+
+def show(id: int, db: Session):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with the id {id} is not available")
+    return user
+
+
+def update_user(id: int, request: schemas.User, db: Session):
+    user = db.query(models.User).filter(models.User.id == id)
+
+    if not user.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id {id} not found")
+
+    user.update(request.dict())
+    db.commit()
+    return 'updated'
+
+
+def destroy(id: int, db: Session):
+    user = db.query(models.User).filter(models.User.id == id)
+
+    if not user.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id {id} not found")
+
+    user.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status, HTTPException
+from typing import List
 from repository import user
 from sqlalchemy.orm import Session
 import schemas
@@ -12,6 +13,22 @@ router = APIRouter(
 
 get_db = database.get_db
 
-@router.post('/', response_model=schemas.ShowUser)
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     return user.create(request, db)
+
+@router.get('/', response_model=List[schemas.ShowUser])
+def list_users(db: Session = Depends(get_db)):
+    return user.get_all(db)
+
+@router.get('/{id}', response_model=schemas.ShowUser)
+def get_user(id: int, db: Session = Depends(get_db)):
+    return user.show(id, db)
+
+@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_user(id: int, request: schemas.User, db: Session = Depends(get_db)):
+    return user.update_user(id, request, db)
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    return user.destroy(id, db)
